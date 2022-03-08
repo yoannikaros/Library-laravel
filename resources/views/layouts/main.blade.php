c69bc09f44e5a8b51a0997d06370382c1f2fa20e
YOAN <yoannikaros29@gmail.com>
2022-03-08 13:19:52 +0700

    Perubahan

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,8 +12,8 @@
   </head>
   <body>
     @include('layouts.partials.navbar')
-    @yield('content')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    @yield('content') // this is the content of the page
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
   </body>
 </html>

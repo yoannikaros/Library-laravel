695419ad0dc8482b6412b08abfadace00f9d059e
farkmu45 <farukmicros@outlook.com>
2022-03-09 09:16:37 +0700

    Don't show single staff in staff resource route

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,9 @@ Route::prefix('admin')->group(function () {
     Route::get('/login', [AdminController::class, 'login'])->middleware('guest')->name('adminLogin');
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
     Route::resource('books', BookController::class);
-    Route::resource('books', BookController::class);
     Route::resource('reservations', ReservationController::class);
     Route::resource('reversions', ReversionController::class);
-    Route::resource('staffs', StaffController::class);
+    Route::resource('staffs', StaffController::class)->except('show');
 });
 
 Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');

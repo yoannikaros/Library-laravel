0985df667267f74b57dd25be4efcb86a514f7294
farkmu45 <farukmicros@outlook.com>
2022-03-09 15:14:14 +0700

    Use wildcard in request path

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,16 +10,16 @@
     <li class="menu-header">Staff</li>
     <li class="{{ request()->routeIs('dashboard') ? 'active' : '' }}"><a class="nav-link" href="{{ route('dashboard') }}"><i
           class="fas fa-columns"></i> <span>Dasbor</span></a></li>
-    <li class="{{ request()->routeIs('books.index') ? 'active' : '' }}"><a href="{{ route('books.index') }}"><i
+    <li class="{{ request()->routeIs('books.*') ? 'active' : '' }}"><a href="{{ route('books.index') }}"><i
           class="fas fa-book"></i> <span>Buku</span></a></li>
-    <li class="{{ request()->routeIs('staffs.index') ? 'active' : '' }}"><a href="{{ route('staffs.index') }}"><i
+    <li class="{{ request()->routeIs('staffs.*') ? 'active' : '' }}"><a href="{{ route('staffs.index') }}"><i
           class="fas fa-people-carry"></i> <span>Manajemen Staf</span></a></li>
 
     <li class="menu-header">Member</li>
-    <li class="{{ request()->routeIs('reservations.index') ? 'active' : '' }}">
-      <a href="{{ route('reservations.index') }}"><i class="fas fa-book-open"></i> <span>Peminjaman</span></a>
+    <li class="{{ request()->routeIs('reservations.*') ? 'active' : '' }}">
+      <a href="{{ route('reservations.*') }}"><i class="fas fa-book-open"></i> <span>Peminjaman</span></a>
     </li>
-    <li class="{{ request()->routeIs('reversions.index') ? 'active' : '' }}"><a
+    <li class="{{ request()->routeIs('reversions.*') ? 'active' : '' }}"><a
         href="{{ route('reversions.index') }}"><i class="fas fa-school"></i>
         <span>Pengembalian</span></a></li>
   </ul>
